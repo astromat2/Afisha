54bd0349602cde916c3e6742e413f81152e13932
astromat2 <astromat_01@mail.ru>
2023-02-15 20:14:37 +0600

    hw1

---
+++ movie_app/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Director, Movie, Review
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['id', 'name']
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer()
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'name', 'description', 'duration', 'director']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'text', 'movie']
